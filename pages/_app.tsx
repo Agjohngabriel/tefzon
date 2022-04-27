5846d02f73708d2996197f5b0e7e962294f2d2c3
agjohngabriel <agjohngabriel@gmail.com>
2022-04-26 23:07:27 -0400

    installed and downloaded the neccessary fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap"
           rel="stylesheet"
