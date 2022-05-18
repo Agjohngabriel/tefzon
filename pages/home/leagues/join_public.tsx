e1c1c8dd4bf43a9751223061529723b373d76176
agjohngabriel <agjohngabriel@gmail.com>
2022-05-18 16:15:00 -0400

    Consumed login Api and Signup API step two

--- pages/home/leagues/join_public.tsx
+++ pages/home/leagues/join_public.tsx
@@ -1,7 +1,9 @@
 import MainLayout from "../../../components/MainLayout";
 
 const JoinPublic = () => {
-  return <MainLayout></MainLayout>;
+  return <MainLayout>
+    <h1>uguigig</h1>
+  </MainLayout>;
 };
 
 export default JoinPublic;
