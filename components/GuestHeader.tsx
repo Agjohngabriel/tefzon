cb06e4a2d15b45e12319834005e06778f42c5742
shieldstring <edmunddaniels06@gmail.com>
2022-06-11 18:08:00 +0100

    fixed guest header responsiveness

--- components/GuestHeader.tsx
+++ components/GuestHeader.tsx
@@ -1,60 +1,76 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 function GuestHeader() {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
   return (
-    <nav className="w-full bg-white shadow z-10">
-      <div className="container mx-auto px-6 py-3 lg:px-20 flex items-center justify-between flex-wrap">
-        <div className="flex items-center flex-shrink-0 text-gray-600 mr-6">
-          <Link href="/" passHref>
-            <img src="/brand.png" alt="logo" width="150px" />
+    <nav className="flex items-center flex-wrap mx-auto px-6 py-3 lg:px-20  bg-green-400 p-3 ">
+      <Link href="/" passHref>
+        <a className="inline-flex items-center p-2 mr-4 ">
+          <img src="/brand.png" alt="logo" width="150px" />
+        </a>
+      </Link>
+      <button
+        className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-gray-600 border-gray-600 ml-auto hover:text-white outline-none"
+        onClick={handleClick}
+      >
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
+
+      <div
+        className={`${
+          active ? "" : "hidden"
+        }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+      >
+        <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+          <Link href="" passHref>
+            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 under hover:text-blue-500 font-bold items-center justify-center hover:bg-green-600 ">
+              Home
+            </a>
           </Link>
-        </div>
-        <div className="block md:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-gray-600 border-gray-600">
-            <svg
-              className="fill-current h-4 w-4"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div className="w-full block flex-grow md:flex md:justify-end md:w-auto">
-          <div>
-            <Link href="" passHref>
-              <span className="font-arcon block md:inline-block text-sm px-1 lg:px-3 py-2 leading-none rounded text-gray-600 under hover:text-blue-500 mt-4 md:mt-0 cursor-pointer">
-                Home
-              </span>
-            </Link>
-            <Link href="" passHref>
-              <span className="font-arcon block md:inline-block text-sm px-1 lg:px-3 py-2 leading-none rounded text-gray-600 hover:text-blue-500 mt-4 md:mt-0 cursor-pointer">
-                Price
-              </span>
-            </Link>
-            <Link href="" passHref>
-              <span className="font-arcon block md:inline-block text-sm px-1 lg:px-3 py-2 leading-none rounded text-gray-600 hover:text-blue-500 mt-4 md:mt-0 cursor-pointer">
-                Scout
-              </span>
-            </Link>
-            <Link href="" passHref>
-              <span className="font-arcon block md:inline-block text-sm px-1 lg:px-3 py-2 leading-none rounded text-gray-600 hover:text-blue-500 mt-4 md:mt-0 cursor-pointer">
-                Statistics
-              </span>
-            </Link>
-            <Link href="" passHref>
-              <span className="font-arcon block md:inline-block text-sm px-1 lg:px-3 py-2 leading-none rounded text-gray-600 hover:text-blue-500 mt-4 md:mt-0 cursor-pointer">
-                Help
-              </span>
-            </Link>
-            <Link href="" passHref>
-              <span className="font-arcon block md:inline-block text-sm px-1 lg:px-3 py-2 leading-none rounded text-gray-600 hover:text-blue-500 mt-4 md:mt-0 cursor-pointer">
-                Contact
-              </span>
-            </Link>
-          </div>
+          <Link href="" passHref>
+            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 under hover:text-blue-500 font-bold items-center justify-center hover:bg-green-600 ">
+            Price
+            </a>
+          </Link>
+          <Link href="" passHref>
+            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 under hover:text-blue-500 font-bold items-center justify-center hover:bg-green-600 ">
+            Scout
+            </a>
+          </Link>
+          <Link href="" passHref>
+            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 under hover:text-blue-500 font-bold items-center justify-center hover:bg-green-600 ">
+            Statistics
+            </a>
+          </Link>
+          <Link href="" passHref>
+            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 under hover:text-blue-500 font-bold items-center justify-center hover:bg-green-600 ">
+            Help
+            </a>
+          </Link>
+          <Link href="" passHref>
+            <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-600 under hover:text-blue-500 font-bold items-center justify-center hover:bg-green-600 ">
+              Contact
+            </a>
+          </Link>
         </div>
       </div>
     </nav>
