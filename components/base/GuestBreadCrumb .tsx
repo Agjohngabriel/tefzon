4de7edc3fe18ec27f9479333ce19f4d905b3958e
shieldstring <edmunddaniels06@gmail.com>
2022-10-21 16:14:06 +0100

    fixed guest breadcrumb responsiveness

--- components/base/GuestBreadCrumb .tsx
+++ components/base/GuestBreadCrumb .tsx
@@ -4,7 +4,7 @@ const GuestBreadCrumb = () => {
       <div className="bg-violet-500  lg:pt-[20px] md:pt-[5px] pt-3 justify-between">
 
         <div className="flex justify-between ">
-          <div className="ml-8 sm:ml-20 w-1/2 mt-10 sm:mt-16  lg:mb-12">
+          <div className="ml-8 sm:ml-20 w-1/2 mt-2 sm:mt-16  lg:mb-12">
             <h2 className="font-oswald text-gray-100 font-bold text-xl sm:text-2xl lg:text-5xl tracking-tighter leading-tight">
             Tefzon Fantasy
             </h2>
