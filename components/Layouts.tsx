4570c6842c7c186a43119f51bea30d7a3d37d3b3
shieldstring <edmunddaniels06@gmail.com>
2022-10-21 15:09:57 +0100

    added guest breadcrumb

--- components/Layouts.tsx
+++ components/Layouts.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
-import AccountBreadCrumb from "./base/AccountBreadCrumb";
-import BreadCrumb from "./base/BreadCrumb";
+import GuestBreadCrumb from "./base/GuestBreadCrumb ";
 import GuestHeader from "./GuestHeader";
 import MainFooter from "./MainFooter";
 
@@ -9,7 +8,7 @@ export default function Layout(props: { children: ReactNode }) {
     <>
       <GuestHeader />
       {/* <BreadCrumb /> */}
-      <AccountBreadCrumb />
+      <GuestBreadCrumb />
       <div className="bg-[#E4ECFB] shadow-inner">
         {/* <div className="container bg-gradient-to-br from-[#FFFFFF]/100 via-[#F2F6FF]/50 to-[#E5ECFA]/100 border-inherit rounded-xl shadow-2xl shadow-indigo-500/50 mx-auto mt-10 mb-20  px-4 py-6 lg:px-20  w-auto"> */}
         {props.children}
