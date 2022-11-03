f8785c4fc3119cf638bdff0dfc51d1bc20d59cb0
shieldstring <edmunddaniels06@gmail.com>
2022-11-03 16:43:56 +0100

    updated single UI and added sportmonk api to env

--- next.config.js
+++ next.config.js
@@ -7,5 +7,7 @@ module.exports = {
   nextConfig,
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
+    SPORTS_URL:process.env.SPORTS_URL,
+    SPORTS_APIKEY:process.env.SPORTS_APIKEY,
   },
 };
--- pages/home/news/single.tsx
+++ pages/home/news/single.tsx
@@ -5,45 +5,25 @@ const Index = () => {
   return (
     <Layout>
       <main className="pt-10">
-        <div className="mb-4 md:mb-0 w-full max-w-screen-lg mx-auto relative">
-          <div
-            className="absolute left-0 bottom-0 w-full h-full z-10"
-            style={{
-              ["background-image" as any]:
-                "linear-gradient(180deg,transparent,rgba(0,0,0,.7))",
-            }}
-          ></div>
-          <img
-            src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
-            className="absolute left-0 top-0 w-full h-full z-0 object-cover"
-            alt=""
-          />
-          <div className="p-4 absolute bottom-0 left-0 z-20">
-            <a
-              href="#"
-              className="px-4 py-1 bg-black text-gray-200 inline-flex items-center justify-center mb-2"
-            >
-              Nutrition
-            </a>
-            <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
+        <div className="mb-4 md:mb-0 w-full mx-auto relative px-10 lg:px-28">
+          <div className="px-4 lg:px-0">
+            <h2 className="text-4xl font-semibold text-gray-800 leading-tight">
               Pellentesque a consectetur velit, ac molestie ipsum. Donec
               sodales, massa et auctor.
             </h2>
-            <div className="flex mt-3">
-              <img
-                src="https://randomuser.me/api/portraits/men/97.jpg"
-                className="h-10 w-10 rounded-full mr-2 object-cover"
-                alt=""
-              />
-              <div>
-                <p className="font-semibold text-gray-200 text-sm">
-                  {" "}
-                  Mike Sullivan{" "}
-                </p>
-                <p className="font-semibold text-gray-400 text-xs"> 14 Aug </p>
-              </div>
-            </div>
+            <a
+              href="#"
+              className="py-2 text-green-700 inline-flex items-center justify-center mb-2"
+            >
+              Cryptocurrency
+            </a>
           </div>
+
+          <img
+            src="https://images.unsplash.com/photo-1587614387466-0a72ca909e16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+            alt=""
+            className="w-full object-cover lg:rounded h-[12em]"
+          />
         </div>
 
         <div className="flex flex-col lg:flex-row lg:space-x-12 mx-auto px-28 ">
