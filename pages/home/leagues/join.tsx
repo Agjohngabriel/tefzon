e5e220d01cebcdcb1df99db2cdc429380c5e1708
shieldstring <edmunddaniels06@gmail.com>
2023-02-20 03:20:42 +0100

    fixed join league ui

--- pages/home/leagues/join.tsx
+++ pages/home/leagues/join.tsx
@@ -80,7 +80,7 @@ const Join = () => {
                   <div className="col-span-12 sm:col-span-12 md:col-span-12  px-2 py-2">
                     {/* <!-- Start Card List --> */}
                     <div className="rounded-xl ">
-                      <div className="">
+                      <div className="grid grid-cols-1 gap-6   grid-cols-2 mt-6">
                         {/* <!-- Start Navitem --> */}
                         <button
                           onClick={goToPrivate}
